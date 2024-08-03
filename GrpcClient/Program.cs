d57bc92186a65cbbee40e5119076cc5b3a5ea457
Momostafa1997 <mohammed_ahmed97@outlook.com>
2024-08-04 00:17:56 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var request = new HelloRequest { Name = "darsh" };
+
+        var channel = GrpcChannel.ForAddress("https://localhost:7109/");
+
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(request);
+
+        Console.WriteLine(reply);
+
+        Console.ReadLine();
+    }
+}
